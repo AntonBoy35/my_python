48b033fa87191c67b0a67eb6a11ba8c43aa2e1b6
AntonBoy35 <107911384+AntonBoy35@users.noreply.github.com>
2022-06-22 20:27:46 +0300

    Update geraldic.py
    оформление

--- geraldic.py
+++ geraldic.py
@@ -1,20 +1,10 @@
-# Задание: геральдические символы Москвы
-'''
-Сгенерируйте PDF документ из списка флагов и гербов районов Москвы:
-https://video.ittensive.com/python-advanced/data-102743-2019-11-13.utf.csv
-На каждой странице документа выведите название геральдического символа (Name), его описание (Description) и его изображение (Picture).
-Для показа изображений используйте адрес
-https://op.mos.ru/MEDIA/showFile?id=XXX
-где XXX - это значение поля Picture в наборе данных. Например:
-https://op.mos.ru/MEDIA/showFile?id=8466da35-6801-41a9-a71e-04b60408accb
-В случае возникновения проблем с загрузкой изображений с op.mos.ru можно добавить в код настройку для форсирования использования дополнительных видов шифрования в протоколе SSL/TLS.
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
-'''
 import pandas as pd
 import pdfkit
+
 # загрузка данных из csv файла
 data = pd.read_csv("http://video.ittensive.com/python-advanced/data-102743-2019-11-13.utf.csv", delimiter=";")
-# Создание html документа
+
+# Создание автоматического html документа
 html = '''<html>
 <head>
     <title>Геральдические символы Москвы</title>
@@ -33,7 +23,7 @@ for i, item in data.iterrows():
     html += '<p style="font-size:150%">' + item['Description'] + '</p>'
 html += '</body></html>'
 
-# настройка документа
+# настройка pdf документа
 config = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
 options = {
     'page-size': 'A4',
