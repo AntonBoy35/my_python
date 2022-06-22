7cf480395ab63199bf208bd720a8be642e24ebcf
AntonBoy35 <107911384+AntonBoy35@users.noreply.github.com>
2022-06-22 20:59:53 +0300

    Update getting_quotes.py
    оформление

--- getting_quotes.py
+++ getting_quotes.py
@@ -1,10 +1,3 @@
-# Задание: получение котировок акций
-'''
-Получите данные по котировкам акций со страницы:
-mfd.ru/marketdata/?id=5&group=16&mode=3&sortHeader=name&sortOrder=1&selectedDate=01.11.2019
-и найдите, по какому тикеру был максимальный рост числа сделок (в процентах) за 1 ноября 2019 года.
-'''
-
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -15,30 +8,32 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 # Сброс ограничений на количество символов в записи
 pd.set_option('display.max_colwidth', None)
-# загружаем данные о котировках
+
+# загрузка данных о котировках
 res = requests.get('https://mfd.ru/marketdata/?id=5&group=16&mode=3&sortHeader=name&sortOrder=1&selectedDate=01.11.2019')
-# данные из запроса обрабатываем с помощью Beautifulsoup в виде html контента
+# данные из запроса обрабатываются с помощью Beautifulsoup в виде html контента
 html = BeautifulSoup(res.content, features="lxml")
+# выборка части кода включающего тег <table> с названием класса в котором заключается информация с котировками
 table = html.find('table', {'class': 'mfd-table'})
-# создаем пустой список
+# пустой список
 rows = []
-trs = table.find_all('tr')
-# перебираем внутри тега table все теги tr и внурти тегов tr теги td и добавляем данные в пустой список
+trs = table.find_all('tr') # выборка части кода с тегами <tr> из <table>
+# выборка текстовой информации из полученной html таблицы в список
 for tr in trs:
     tr = [td.get_text(strip=True) for td in tr.find_all('td')]
     if len(tr) > 0:
         rows.append(tr)
 # загружаем данные из списка во фрейм pandas с названием столбцов
 data = pd.DataFrame(rows, columns=['Тикер', 'Дата', 'Сделки_сумма', 'Сделки_ед', 'Сделки_%', 'Перед_закр', 'откр', 'мин', 'макс', 'срвзв', 'объем_шт', 'объем_руб', 'кол_сделок'], )
-# выбираем строки, где Сделки_сумма не равно N/A
+# выборка данных по сумме сделок с заполненными строками
 data = data[data['Сделки_сумма'] != 'N/A']
-# убираем лишние знаки
+# замена мешающих символов
 data['Сделки_%'] = data['Сделки_%'].str.replace('−', '-').str.replace('%', '').astype('float')
-# назначаем индекс
+# назначение индекса
 data = data.set_index('Сделки_%')
-# сортируем по индексу в порядке убывания
+# сортировка по индексу в порядке убывания
 data = data.sort_index(ascending=False)
-# убираем индекс
+# сброс индекса
 data = data.reset_index()
-# выводим тикер первой строки (ответ)
+# вывод информации по тикеру первой строки (ответ)
 print(data['Тикер'][0:1])
